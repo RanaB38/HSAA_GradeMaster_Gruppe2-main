62a6e8988fdc456dea648ccfa8ab446e3971c9d6
Evren Kacar <evren_kacar54@hotmail.com>
2024-11-06 16:06:19 +0100

    Einfügen des ersten Kurses wurde geändert. Beim Einfügen des ersten Kurses wurde die ID mit übergeben dies führte bei einem Post eines neuen Kurses zu einem Fehler da die ID automatisch erstellt wird.

--- HSAA_GradeMaster_TeamName-main/grade-master-service/src/main/resources/data.sql
+++ HSAA_GradeMaster_TeamName-main/grade-master-service/src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO STUDENT (id, name, email) VALUES ('86169', 'Emre Hirka', '86169@stud
 INSERT INTO STUDENT (id, name, email) VALUES ('83128', 'Emre Tülü', '83128@studmail.htw-aalen.de');
 INSERT INTO STUDENT (id, name, email) VALUES ('83061', 'Evren Kacar', '83061@studmail.htw-aalen.de');
 
-INSERT INTO KURS (id, name, description) VALUES ('1', 'Projektseminar Programmierprojekt', 'Ein Projekt welches in Gruppenarbeit absolviert
+INSERT INTO KURS ( name, description) VALUES ( 'Projektseminar Programmierprojekt', 'Ein Projekt welches in Gruppenarbeit absolviert
 wird. Jede Gruppe lernt die Tools kennen, um eine Webanwendung zu programmieren.')
