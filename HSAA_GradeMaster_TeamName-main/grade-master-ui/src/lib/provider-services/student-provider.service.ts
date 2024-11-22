90f570036a16a6eeadaad46d3e8d830b9f33b02a
Evren Kacar <evren_kacar54@hotmail.com>
2024-11-22 21:54:37 +0100

    Studenten werden von der Datenbank geholt und können auch hinzugefügt werden

--- HSAA_GradeMaster_TeamName-main/grade-master-ui/src/lib/provider-services/student-provider.service.ts
+++ HSAA_GradeMaster_TeamName-main/grade-master-ui/src/lib/provider-services/student-provider.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Student } from '../domain/student.interfaces';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentProviderService {
-
 
   // Internes BehaviorSubject zum Verwalten der Student-Liste
   private studentsSubject: BehaviorSubject<Student[]> = new BehaviorSubject<Student[]>([]);
@@ -15,19 +15,32 @@ export class StudentProviderService {
   // Subjects sollten nie driekt rausgegeben werden -> Manipulationsgefahr
   public students$: Observable<Student[]> = this.studentsSubject.asObservable();
 
-  constructor() {
+  private baseUrl = 'http://localhost:8080/api/v1/student';
 
-    // Initialisierung im Konstruktor, wäre in "ngOnInit()" ebenfalls möglich
+  constructor(private httpClient: HttpClient) {
+
+    this.loadStudents();
+    /* Initialisierung im Konstruktor, wäre in "ngOnInit()" ebenfalls möglich
     const initialStudents: Student[] = [
       { id: 1, name: 'Emre', email: 'hans@test.de' },
       { id: 2, name: 'Helmut', email: 'helmut@test.de' },
       { id: 3, name: 'Friedrich', email: 'friedrich@test.de' },
       { id: 4, name: 'Josef', email: 'josef@test.de' },
     ];
-    this.studentsSubject.next(initialStudents);
+    this.studentsSubject.next(initialStudents);*/
 
    }
 
+  private loadStudents(): void {
+    this.httpClient.get<Student[]>(this.baseUrl).subscribe({
+      next: (students) => {
+        this.studentsSubject.next(students);
+      },
+      error: (err) => {
+        console.error('Fehler beim Laden der Studenten:', err);
+      }
+    });
+  }
 
   // GET Students
   public getAllStudents(): Observable<Student[]> {
@@ -36,20 +49,25 @@ export class StudentProviderService {
 
   // POST Student
   public createStudent(student: Student): void {
-
     console.log('>>> ', student);
 
-    const currentStudents = this.studentsSubject.value;
-    const updatedStudents = [...currentStudents, student]; // Neuen Studenten hinzufügen; Spread-Operator
-    this.studentsSubject.next(updatedStudents);
+    // POST Student
+    this.httpClient.post<Student>(this.baseUrl, student).subscribe({
+      next: (createdStudent) => {
+        const currentStudents = this.studentsSubject.value;
+        const updatedStudents = [...currentStudents, createdStudent]; // Neuen Studenten hinzufügen
+        this.studentsSubject.next(updatedStudents);
 
-    console.log('Aktualisierte Studentenliste:', updatedStudents);
+        console.log('Aktualisierte Studentenliste:', updatedStudents);
+      },
+      error: (err) => {
+        console.error('Fehler beim Erstellen des Studenten:', err);
+      }
+    });
   }
 
   public getStudentById(id: number): Student | undefined {
     const currentStudents = this.studentsSubject.getValue(); // Holt aktuelle Liste
     return currentStudents.find(student => student.id === id); // Filtert nach ID
   }
-
-
 }
