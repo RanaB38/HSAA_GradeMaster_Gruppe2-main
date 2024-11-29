ef57e866a968433da87332ca74113e812b6b4654
Evren Kacar <evren_kacar54@hotmail.com>
2024-11-29 01:12:13 +0100

    Kurs ID, Name und beschreibung werden nun aus der Datenbank geholt und auf der Seite angezeigt

--- HSAA_GradeMaster_TeamName-main/grade-master-ui/src/app/views/course/course-detail/course-detail.component.ts
+++ HSAA_GradeMaster_TeamName-main/grade-master-ui/src/app/views/course/course-detail/course-detail.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import {AsyncPipe, NgIf} from "@angular/common";
 import {MatCardContent} from "@angular/material/card";
-import {Observable} from "rxjs";
 import {Course} from "../../../../lib/domain/course.interfaces";
+import {ActivatedRoute} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {CourseCoreService} from "../../../../lib/core-services/course-core.service";
+import {MaterialColor} from "../../../../lib/enums/material-color";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-course-detail',
@@ -10,14 +14,36 @@ import {Course} from "../../../../lib/domain/course.interfaces";
   imports: [
     AsyncPipe,
     NgIf,
-    MatCardContent
+    MatCardContent,
+    MatToolbar
   ],
   templateUrl: './course-detail.component.html',
   styleUrl: './course-detail.component.scss'
 })
 export class CourseDetailComponent {
-  public dataSource$!: Observable<Course[]>; // "!" oder "... | undefined"
+  public dataSource$!: Course | undefined; // "!" oder "... | undefined"
+  public title = 'Kurs Details';
+  public color: MaterialColor = 'accent';
+  public courseId!: number;
 
+  constructor(
+    private courseCoreService: CourseCoreService,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+  ) {
+    this.ngOnInit()
+    this.http.get("http://localhost:8080/api/v1/course/"+this.courseId).subscribe(c =>
+      { console.log(c);}
+    );
+  }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.courseId = parseInt(<string>params.get('id'));
+      this.loadCourseDetails();
+    });
+  }
 
-
+  loadCourseDetails(): void {
+    this.dataSource$ = this.courseCoreService.getCourse(this.courseId);
+ }
 }
--- HSAA_GradeMaster_TeamName-main/grade-master-ui/src/lib/domain/course.interfaces.ts
+++ HSAA_GradeMaster_TeamName-main/grade-master-ui/src/lib/domain/course.interfaces.ts
@@ -5,4 +5,5 @@ export interface Course {
     id: number;
     name: string;
     description: string;
+   // students: Student[];
 }
