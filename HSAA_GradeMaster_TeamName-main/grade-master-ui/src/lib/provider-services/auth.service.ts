2960dcc21adf18eef5410649f8fe6bc98f259dc8
Evren Kacar <evren_kacar54@hotmail.com>
2025-01-09 12:18:44 +0100

    Benutzernamen hinzugefügt für eine weitere aufgabe

--- HSAA_GradeMaster_TeamName-main/grade-master-ui/src/lib/provider-services/auth.service.ts
+++ HSAA_GradeMaster_TeamName-main/grade-master-ui/src/lib/provider-services/auth.service.ts
@@ -10,6 +10,7 @@ export class AuthService {
 
   private baseUrl = 'http://localhost:8080/api/public/v1/user';
   private role: 'STUDENT' | 'LECTURER' = 'STUDENT';
+  private username = '';
 
   constructor(private http: HttpClient) {}
 
@@ -22,6 +23,7 @@ export class AuthService {
       tap((response) => {
         const role = response.role ? response.role.replace('ROLE_', '') : 'STUDENT';
         this.setUserRole(role as 'STUDENT' | 'LECTURER'); // Setzt die Rolle korrekt
+        this.setUsername(response.username)
       }),
       catchError((error) => {
         console.error('Fehler bei der Authentifizierung:', error);
@@ -44,6 +46,14 @@ export class AuthService {
 
   getUserRole(): 'STUDENT' | 'LECTURER' {
     return this.role;
+  }
+
+  setUsername(username: string):void{
+    this.username = username;
+  }
+
+  getUsername(): string{
+    return this.username;
   }
 }
 
