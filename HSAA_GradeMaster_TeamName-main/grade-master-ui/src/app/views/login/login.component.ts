965259a1c9797cc169e00e5a74c5b7fb10a0bf96
Evren Kacar <evren_kacar54@hotmail.com>
2025-01-12 17:57:02 +0100

    Nach der anmeldung wird man zu den Kursen weitergeleitet.

--- HSAA_GradeMaster_TeamName-main/grade-master-ui/src/app/views/login/login.component.ts
+++ HSAA_GradeMaster_TeamName-main/grade-master-ui/src/app/views/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../lib/provider-services/auth.service';
 import { CommonModule } from '@angular/common';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,7 @@ export class LoginComponent {
   errorMessage = 'Invalid username or password';
   successMessage = 'Login successful';
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   handleLogin(event: Event): void {
     event.preventDefault();
@@ -30,6 +31,7 @@ export class LoginComponent {
         this.successMessage = `Welcome, ${user.username}!`;
 
         localStorage.setItem('authToken', btoa(`${this.username}:${this.password}`));
+        this.router.navigate(['/courses']);
       },
       (error) => {
         this.invalidLogin = true;
