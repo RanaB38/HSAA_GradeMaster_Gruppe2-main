03d604705b06300c79f16607cb0c2589c38c9e0b
FatmeKhraizat <85910@studmail.htw-aalen.de>
2025-02-12 19:40:13 +0100

    Aufgabe25 Backend

--- HSAA_GradeMaster_TeamName-main/grade-master-service/src/main/java/de/hsaalen/grademaster/grademasterservice/repository/NotenspiegelRepository.java
+++ HSAA_GradeMaster_TeamName-main/grade-master-service/src/main/java/de/hsaalen/grademaster/grademasterservice/repository/NotenspiegelRepository.java
@@ -4,6 +4,9 @@ import de.hsaalen.grademaster.grademasterservice.domain.Notenspiegel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository-Schnittstelle für Notenspiegel-Entitäten.
+ */
 @Repository
 public interface NotenspiegelRepository extends JpaRepository<Notenspiegel, Long> {
 }
