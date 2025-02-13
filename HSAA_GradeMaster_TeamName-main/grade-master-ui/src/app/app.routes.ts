619b6fdf06195183a9c26ee238d4bd4bc4a9e6ed
HirkaE <136345081+HirkaE@users.noreply.github.com>
2025-02-13 14:19:25 +0100

    Readme

--- HSAA_GradeMaster_TeamName-main/grade-master-ui/src/app/app.routes.ts
+++ HSAA_GradeMaster_TeamName-main/grade-master-ui/src/app/app.routes.ts
@@ -1,100 +1,35 @@
 import { Routes } from '@angular/router';
 import { CourseDialogHandlerComponent } from './views/course/course-dialog/course-dialog-handler/course-dialog-handler.component';
-import { LoginComponent } from './views/login/login.component'; // Login-Komponente importieren
+import { LoginComponent } from './views/login/login.component';
 import { AuthGuard } from './views/login/auth.guard';
-import { HomeComponent } from './views/home/home.component'; // Home-Komponente importieren
+import { HomeComponent } from './views/home/home.component';
 
-// @ts-ignore
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login', // Standardroute zur Home-Seite umleiten
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent, // Home-Komponente
-  },
-  {
-    path: 'login',
-    component: LoginComponent, // Login-Komponente
-  },
-  {
-    path: 'dialog',
-    component: CourseDialogHandlerComponent,
-    outlet: 'dialog',
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dialog', component: CourseDialogHandlerComponent, outlet: 'dialog' },
   {
     path: 'courses',
-    loadComponent: () =>
-      import('./views/course/course.component').then((mod) => mod.CourseComponent),
-    canActivate: [AuthGuard], // Nur für authentifizierte Benutzer zugänglich
+    loadComponent: () => import('./views/course/course.component').then(m => m.CourseComponent),
+    canActivate: [AuthGuard],
     children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'overview',
-      },
-      {
-        path: 'overview',
-        loadComponent: () =>
-          import('./views/course/course-overview/course-overview.component').then(
-            (mod) => mod.CourseOverviewComponent
-          ),
-      },
-      {
-        path: ':id/details', // Kurs-Detail-Seite
-        loadComponent: () =>
-          import('./views/course/course-detail/course-detail.component').then(
-            (mod) => mod.CourseDetailComponent
-          ),
-      },
-      {
-        path: ':courseId/bewertungsschema', //Bewertungsschema Edit
-        loadComponent: () =>
-          import('./views/course/bewertungsschema-edit/bewertungsschema-edit.component').then(
-            (mod) => mod.BewertungsschemaEditComponent
-          ),
-      },
-      {
-        path: ':courseId/groups/:groupId/details', // Gruppen-Detail-Seite
-        loadComponent: () =>
-          import('./views/course/group-detail/group-detail.component').then((mod) => {
-            console.log('GroupDetailComponent wird geladen...');
-            return mod.GroupDetailComponent;
-          }),
-      },
-    ],
+      { path: '', pathMatch: 'full', redirectTo: 'overview' },
+      { path: 'overview', loadComponent: () => import('./views/course/course-overview/course-overview.component').then(m => m.CourseOverviewComponent) },
+      { path: ':id/details', loadComponent: () => import('./views/course/course-detail/course-detail.component').then(m => m.CourseDetailComponent) },
+      { path: ':courseId/bewertungsschema', loadComponent: () => import('./views/course/bewertungsschema-edit/bewertungsschema-edit.component').then(m => m.BewertungsschemaEditComponent) },
+      { path: ':courseId/groups/:groupId/details', loadComponent: () => import('./views/course/group-detail/group-detail.component').then(m => m.GroupDetailComponent) }
+    ]
   },
   {
     path: 'students',
-    loadComponent: () =>
-      import('./views/student/student.component').then((mod) => mod.StudentComponent),
-    canActivate: [AuthGuard], // Nur für authentifizierte Benutzer zugänglich
+    loadComponent: () => import('./views/student/student.component').then(m => m.StudentComponent),
+    canActivate: [AuthGuard],
     children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'list',
-      },
-      {
-        path: 'list',
-        loadComponent: () =>
-          import('./views/student/student-list/student-list.component').then(
-            (mod) => mod.StudentListComponent
-          ),
-      },
-      {
-        path: ':id/details',
-        loadComponent: () =>
-          import('./views/student/student-detail/student-detail.component').then(
-            (mod) => mod.StudentDetailComponent
-          ),
-      },
-    ],
+      { path: '', pathMatch: 'full', redirectTo: 'list' },
+      { path: 'list', loadComponent: () => import('./views/student/student-list/student-list.component').then(m => m.StudentListComponent) },
+      { path: ':id/details', loadComponent: () => import('./views/student/student-detail/student-detail.component').then(m => m.StudentDetailComponent) }
+    ]
   },
-  {
-    path: 'notenspiegel',
-    loadComponent: () => import('./views/notenspiegel/notenspiegel.component').then((mod) => mod.NotenspiegelComponent),
-  }
+  { path: 'notenspiegel', loadComponent: () => import('./views/notenspiegel/notenspiegel.component').then(m => m.NotenspiegelComponent) }
 ];
